893554a731cf373bdd634c902492498a4ef1d09f
MontuSherasiya <sherasiyamontu@gmail.com>
2026-07-02 10:57:16 +0530

    config database

---
+++ server/src/server.js
@@ -0,0 +1,12 @@
+import dotenv from 'dotenv';
+import app from './app.js';
+import connectDB from './config/db.js';
+
+const PORT = process.env.PORT || 5000;
+dotenv.config();
+
+connectDB();
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+})
